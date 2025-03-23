c1faaaacc323b0a35c22ae5051c64f5ec5ed06d7
dativardzma <dati.svanidze123@gmail.com>
2025-03-23 07:30:26 +0400

    made little change in requiremnets.txt

--- kanban/kanban/settings.py
+++ kanban/kanban/settings.py
@@ -56,20 +56,6 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React frontend
-    "https://myfrontend.com",  # Deployed frontend
-]
-
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS"
-]
-
 CORS_ALLOW_HEADERS = [
     "content-type",
     "authorization",
